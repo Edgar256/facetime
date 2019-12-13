814498dde814213cd6b0ffc7a6e8c147f9451006
EDGART <edgar@getpoints.com>
2019-12-13 19:24:36 +0300

    validation in the reigster.php

--- register.php
+++ register.php
@@ -1,3 +1,100 @@
+<?php 
+        //Include config file
+        require_once "config.php";
+
+        //Define variables and initialize with empty values
+        $username = $password = $confirm_password = "";
+        $username_err = $password_err = $confirm_password_err = "";
+        $num_rows = "";
+
+        //Processing form data when form is submitted
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            
+            //Validate username
+            if(empty(trim($_POST["username"]))){
+                $username_err = "Please enter a Username.";
+            }else{
+                //Prepare a select statement
+                $sql = "SELECT id FROM users WHERE username = ?";
+
+                if($stmt = $mysqli->prepare($sql)){
+                    //Bind variables to the prepared statement as paramenterrs
+                    $stmt->bind_param("s", $param_username);
+
+                    //Set parameters
+                    $param_username = trim($_POST["username"]);
+
+                    //Attempt to execute the prepared statement
+                    if($stmt->execute()){
+                        //store result
+                        $stmt->store_result();
+
+                        if($stmt->num_rows == 1){
+                            $username_err = "This username is already taken.";
+                        }else{
+                            $username = trim($_POST["username"]);
+                        }
+                    }else{
+                        echo "Oops! Something went wrong. Please try again later";
+                    }
+                }
+
+                //Close statement
+                $stmt->close;
+                
+            }
+
+            //Validate password
+            if(empty(trim($_POST["password"]))){
+                $password_err = "Please enter Password";
+            }elseif(strlen(trim($_POST["password"]))<6){
+                $password_err = "Password must must have at least 6 characters";
+            }else{
+                $password = trim($_POST["password"]);
+            }
+
+            //Validate confirm password
+            if(empty(trim($_POST["confirm_password"]))){
+                $confirm_password_err = "Please confirim password";
+            }else{
+                $confirm_password_err = "Password did not match.";
+            }
+        }
+
+        //Check input errors before inserting in database
+        if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+            //Prepare an insert statement
+            $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+            if($stmt = $mysqli->prepare($sql)){
+                //Bind variables to the prepared statement as parameters
+                $stmt->bind_param("ss", $param_username, $param_password);
+
+                //Set parameters
+                $param_username = $username;
+                $param_password = password_hash($password, PASSWORD_DEFAULT); //hashes password
+
+                //Attempt to execute the prepared statement 
+                if($stmt->execute){
+                    //Redirect to the login page
+                    header("location: login.php");
+                }else{
+                    echo "Something went wrong. Please try again later.";
+                }
+            }
+
+            //Close statement
+            $stmt->close();
+
+
+        }
+        
+        //Close connection
+        $mysqli->close();
+    
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,13 +109,7 @@
 </head>
 <body>
 
-    <?php 
-        //Include config file
-        require_once "config.php";
-
-        //Define variables and initialize with empty values
     
-    ?>
 
     <div class="container">
         <div class="row">
