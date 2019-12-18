f2ac4fe7f126d789f7908125573627a355e445e0
EDGART <edgar@getpoints.com>
2019-12-18 17:49:39 +0300

    created menu file

--- home.php
+++ home.php
@@ -19,54 +19,7 @@
         <div class="container">
         
             <div class="col-sm-2">
-                <div class="menu-tile">
-                    <img src="./assets/images/face002.jpg" class="profile-image-wrap">
-                    <?php echo $_SESSION["username"]; ?>
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-newspaper"></i>
-                    News Feed
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-user-friends"></i>
-                    Friends
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-envelope"></i>
-                    Messenger
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-video"></i>
-                    Videos on Watch
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-users"></i>
-                    Groups
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-layer-group"></i>
-                    Pages
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-calendar-week"></i>
-                    Events
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-smog"></i>
-                    Weather
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-briefcase"></i>
-                    Jobs
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-list"></i>
-                    Friend Lists
-                </div>
-                <div class="menu-tile">
-                    <i class="fa fa-chart-line"></i>
-                    Recent Activity
-                </div>
+                <?php include('./menu.php') ?>
             </div>
             <div class="col-sm-7">
                 <p><?php echo $_SESSION["username"]; ?>'s post  will go here</p>
---
+++ menu.php
@@ -0,0 +1,48 @@
+<div class="menu-tile">
+    <img src="./assets/images/face002.jpg" class="profile-image-wrap">
+    <?php echo $_SESSION["username"]; ?>
+</div>
+<div class="menu-tile">
+    <i class="fa fa-rss-square"></i>
+    News Feed
+</div>
+<div class="menu-tile">
+    <i class="fa fa-users"></i>
+    Friends
+</div>
+<div class="menu-tile">
+    <i class="fa fa-envelope"></i>
+    Messenger
+</div>
+<div class="menu-tile">
+    <i class="fa fa-tv"></i>
+    Videos on Watch
+</div>
+<div class="menu-tile">
+    <i class="fa fa-users"></i>
+    Groups
+</div>
+<div class="menu-tile">
+    <i class="fa fa-file"></i>
+    Pages
+</div>
+<div class="menu-tile">
+    <i class="fa fa-calendar"></i>
+    Events
+</div>
+<div class="menu-tile">
+    <i class="fa fa-cloud"></i>
+    Weather
+</div>
+<div class="menu-tile">
+    <i class="fa fa-briefcase"></i>
+    Jobs
+</div>
+<div class="menu-tile">
+    <i class="fa fa-list"></i>
+    Friend Lists
+</div>
+<div class="menu-tile">
+    <i class="fa fa-id-badge"></i>
+    Recent Activity
+</div>              
