9e3bc89f9e0397c5d1c05679a205a6b53bc20f39
edgar256 <tinka.edgar@gmail.com>
2019-12-19 07:08:24 +0300

    my page component styling

--- my-page.php
+++ my-page.php
@@ -50,17 +50,19 @@
                         <p>10 likes this week</p>
                     </div>
                     <hr>
-                    <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-                        <button type="button" class="btn btn-secondary">1</button>
-                        <button type="button" class="btn btn-secondary">2</button>
+                    <div class="btn-group my-page-btn-group" role="group" aria-label="Button group with nested dropdown">
+                        <button type="button" class="btn btn-secondary btn-promotion">
+                            <i class="fa fa-newspaper-o"></i>
+                            Create Promotion
+                        </button>
 
-                        <div class="btn-group" role="group">
+                        <div class="btn-group my-page-btn-down" role="group">
                             <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
+                            <i class="fa fa-caret-down"></i>
                             </button>
-                            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                <a class="dropdown-item" href="#">Dropdown link</a>
-                                <a class="dropdown-item" href="#">Dropdown link</a>
+                            <div class="dropdown-menu my-page-dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                <a class="dropdown-item my-page-dropdown-item" href="#">Manage Promotions</a>
+                                <a class="dropdown-item my-page-dropdown-item" href="#">Ad Manager</a>
                             </div>
                         </div>
                     </div>
